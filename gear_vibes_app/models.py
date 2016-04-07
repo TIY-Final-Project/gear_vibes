36e52628428058b691e54fa5ac98389382a6abc7
Cuddlemuffin007 <brennonkrogers@gmail.com>
2016-04-06 22:30:57 -0400

    changed dev db to postgres, added review and tag models

--- gear_vibes_app/models.py
+++ gear_vibes_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.postgres.fields import JSONField
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
@@ -9,6 +10,26 @@ class UserProfile(models.Model):
     bio = models.TextField(blank=True)
     profile_photo = models.ImageField(blank=True, null=True)
     joined = models.DateTimeField(auto_now_add=True)
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=25)
+
+
+class Review(models.Model):
+    CATEGORY_CHOICES = (
+        ('cam', 'Cameras'),
+        ('mus', 'Music'),
+        ('mob', 'Mobile')
+    )
+    title = models.CharField(max_length=60)
+    body = models.TextField()
+    author = models.ForeignKey(User)
+    block_quote = models.CharField(max_length=100)
+    video_url = models.TextField()
+    category = models.CharField(max_length=3, choices=CATEGORY_CHOICES)
+    rating = JSONField()
+    tags = models.ManyToManyField(Tag)
 
 
 @receiver(post_save, sender=User)
