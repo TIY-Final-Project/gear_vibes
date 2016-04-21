1d4b27c3b4c565f1cc53290686f4ee66d125e2d9
Cuddlemuffin007 <brennonkrogers@gmail.com>
2016-04-21 18:41:26 -0400

    more config editing

--- gear_vibes/prod_settings.py
+++ gear_vibes/prod_settings.py
@@ -5,8 +5,4 @@ STATICFILES_LOCATION = 'static'
 MEDIAFILES_LOCATION = 'media'
 
 STATICFILES_STORAGE = 'gear_vibes.custom_storages.StaticStorage'
-STATIC_URL = 'https://{}/{}/'.format(AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
-MEDIA_URL = "https://{}/{}/".format(AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
-
-
 DEFAULT_FILE_STORAGE = 'gear_vibes.custom_storages.MediaStorage'
