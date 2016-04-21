4e951f2de964f305b3c90230a27bac161c00d9fa
Cuddlemuffin007 <brennonkrogers@gmail.com>
2016-04-21 15:32:31 -0400

    added configuration for aws s3 bucket

--- gear_vibes/settings.py
+++ gear_vibes/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'social.apps.django_app.default',
-    'gear_vibes_app'
+    'gear_vibes_app',
+    'storages'
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -183,3 +184,21 @@ SOCIAL_AUTH_LOGIN_URL = '/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+AWS_STORAGE_BUCKET_NAME = 'gear-vibes'
+AWS_ACCESS_KEY_ID = 'AKIAJX4FVGF2AUKVKTEQ'
+AWS_SECRET_ACCESS_KEY = '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'
+
+# Tell django-storages that when coming up with the URL for an item in S3 storage, keep
+# it simple - just use this domain plus the path. (If this isn't set, things get complicated).
+# This controls how the `static` template tag from `staticfiles` gets expanded, if you're using it.
+# We also use it in the next setting.
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+# This is used by the `static` template tag from `static`, if you're using that. Or if anything else
+# refers directly to STATIC_URL. So it's safest to always set it.
+STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+
+# Tell the staticfiles app to use S3Boto storage when writing the collected static files (when
+# you run `collectstatic`).
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
