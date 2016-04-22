18c93017baff77cf8a1e0f8bc52e2a8355717f63
Cuddlemuffin007 <brennonkrogers@gmail.com>
2016-04-21 21:14:40 -0400

    updated settings for aws retry

--- gear_vibes/settings.py
+++ gear_vibes/settings.py
@@ -182,5 +182,9 @@ SOCIAL_AUTH_LOGIN_REDIRECT_URL = '/api/login/'
 SOCIAL_AUTH_LOGIN_ERROR_URL = '/api/login/'
 SOCIAL_AUTH_LOGIN_URL = '/'
 
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_KEY')
+AWS_STORAGE_BUCKET_NAME = 'gear-vibes'
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
